eb6b2906bf6884d3d04d9a61642a896bc6809481
pranomostro <pranomestro@gmail.com>
2016-10-11 15:37:51 +0200

    Fixed null free.

--- nal/nalcat.c
+++ nal/nalcat.c
@@ -8,10 +8,13 @@ const size_t INITLEN=8192;
 int main(void)
 {
 	size_t len=INITLEN;
-	char* s=calloc(len, sizeof(char));
+	char* s=calloc(len, sizeof(char)), * rp;
 
-	while((s=nalread(s, &len, stdin))!=NULL)
+	while((rp=nalread(s, &len, stdin))!=NULL)
+	{
+		s=rp;
 		fputs(s, stdout);
+	}
 	free(s);
 	return 0;
 }
