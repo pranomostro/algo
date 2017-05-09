ed3844a875d625a61e33f7199c705a290e200e0a
pranomostro <pranomestro@gmail.com>
2017-05-09 12:33:55 +0200

    Cleaned and applied new makefile.

--- nal/config.mk
+++ nal/config.mk
@@ -1,10 +1,45 @@
-#configuration makefile
+PREFIX=/usr/local
 
-BENCHFILES=data/single/1 data/single/2 data/single/3 data/single/4 data/single/5 data/single/6 data/single/7 data/single/8 data/single/9 data/single/10 data/single/11 data/single/12 data/single/13 data/single/14 data/single/15 data/single/16 data/single/17 data/single/18 data/single/19 data/single/20 data/single/21 data/single/22 data/single/23 data/single/24 data/single/25 data/single/26 data/gen/zzz data/gen/zzzz data/gen/zzzzz data/change/growing data/change/shrinking
+CC=cc
+AR=ar
+RANLIB=ranlib
 
-CFLAGS=-Wall -Wextra -pedantic -g
-LDFLAGS=-g
+CFLAGS=-Wall -Wextra -std=c99 -pedantic -O2
+LDFLAGS=-s
 
-TARGET=nalfg nalgl nalns nalsc
-OBJ=nalcat.o nalfg.o nalgl.o nalns.o nalsc.o reallocarray.o
-CONF=nal.h config.mk
+BIN=nalfg nalgl nalns nalsc
+OBJ=$(BIN:=.o) nalcat.o reallocarray.o
+CONF=config.mk nal.h
+
+BENCHFILES=data/single/1 \
+data/single/2 \
+data/single/3 \
+data/single/4 \
+data/single/5 \
+data/single/6 \
+data/single/7 \
+data/single/8 \
+data/single/9 \
+data/single/10 \
+data/single/11 \
+data/single/12 \
+data/single/13 \
+data/single/14 \
+data/single/15 \
+data/single/16 \
+data/single/17 \
+data/single/18 \
+data/single/19 \
+data/single/20 \
+data/single/21 \
+data/single/22 \
+data/single/23 \
+data/single/24 \
+data/single/25 \
+data/single/26 \
+data/gen/zzz \
+data/gen/zzzz \
+data/gen/zzzzz \
+data/change/growing \
+data/change/shrinking
+
