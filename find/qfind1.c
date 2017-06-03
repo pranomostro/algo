164c665bf3b61c9b7654fcfe2ae8f6f0c216c775
pranomostro <pranomostro@posteo.net>
2017-06-04 01:54:00 +0200

    Moved a part of ifind1() into qfind1().

--- find/qfind1.c
+++ find/qfind1.c
@@ -15,5 +15,27 @@ size_t qfind1(uint32_t key, uint32_t* data, size_t len)
 
 	for(low=0, high=step; high<len&&data[high]<key; low=high, high+=step);
 	high=high<len?high:len-1;
-	return low+ifind1(key, data+low, high-low);
+
+	size_t mid=midcalc(key, data, high, low);
+
+        while(low<=high&&data[mid]!=key)
+        {
+                if(data[low]>key)
+                {
+                        mid=low-1;
+                        break;
+                }
+                else if(data[high]<key)
+                {
+                        mid=high+1;
+                        break;
+                }
+
+                mid=midcalc(key, data, high, low);
+                if(data[mid]<key)
+                        low=mid+1;
+                else
+                        high=mid-1;
+        }
+        return data[mid]<key?mid+1:mid;
 }
