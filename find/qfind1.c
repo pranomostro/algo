60e24af7cbe8cbf8184316df9b75ce9eaf0525af
pranomostro <pranomostro@posteo.net>
2017-06-04 01:27:14 +0200

    Fixed an error with the list length.

--- find/qfind1.c
+++ find/qfind1.c
@@ -14,5 +14,6 @@ size_t qfind1(uint32_t key, uint32_t* data, size_t len)
 	size_t step=sqrt(len), low, high;
 
 	for(low=0, high=step; high<len&&data[high]<key; low=high, high+=step);
+	high=high<len?high:len-1;
 	return low+ifind1(key, data+low, high-low);
 }
