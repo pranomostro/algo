32a161f29a7a7e6343719a9442d572288e65c235
pranomostro <pranomostro@posteo.net>
2017-10-23 17:40:38 +0200

    Whitespace fix.

--- find/qfind1.c
+++ find/qfind1.c
@@ -18,24 +18,24 @@ size_t qfind1(uint32_t key, uint32_t* data, size_t len)
 
 	size_t mid=midcalc(key, data, high, low);
 
-        while(low<=high&&data[mid]!=key)
-        {
-                if(data[low]>key)
-                {
-                        mid=low-1;
-                        break;
-                }
-                else if(data[high]<key)
-                {
-                        mid=high+1;
-                        break;
-                }
+	while(low<=high&&data[mid]!=key)
+	{
+		if(data[low]>key)
+		{
+			mid=low-1;
+			break;
+		}
+		else if(data[high]<key)
+		{
+			mid=high+1;
+			break;
+		}
 
-                mid=midcalc(key, data, high, low);
-                if(data[mid]<key)
-                        low=mid+1;
-                else
-                        high=mid-1;
-        }
-        return data[mid]<key?mid+1:mid;
+		mid=midcalc(key, data, high, low);
+		if(data[mid]<key)
+			low=mid+1;
+		else
+			high=mid-1;
+	}
+	return data[mid]<key?mid+1:mid;
 }
